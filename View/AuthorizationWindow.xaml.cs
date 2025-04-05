7f542b1d5bd380762a1b42244934f4affd8ec6c0
artxmix <artem.artusevskij01@gmail.com>
2025-04-05 09:47:09 +0300

    Доделал окно регистраци и вынес его на отдельную Page

--- View/AuthorizationWindow.xaml.cs
+++ View/AuthorizationWindow.xaml.cs
@@ -1,13 +1,31 @@
-﻿using System.Windows;
+﻿using FoxVill.View.Pages;
+using FoxVill.ViewModel;
+using System.Windows;
+using System.Windows.Input;
 
 namespace FoxVill.View;
 
 public partial class AuthorizationWindow : Window
 {
+    private readonly AuthorizationWindowViewModel _viewModel;
+
     public AuthorizationWindow()
     {
         InitializeComponent();
 
-        
+        _viewModel = new AuthorizationWindowViewModel();
+
+        this.DataContext = _viewModel;
+
+        var authPage = new AuthPage(this);
+        MainFrame.Navigate(authPage);
+    }
+
+    private void DragModeWindow(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        if (e.ButtonState == MouseButtonState.Pressed)
+        {
+            this.DragMove();
+        }
     }
 }
---
+++ View/Pages/AuthPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FoxVill.View.Pages;
+
+public partial class AuthPage : Page
+{
+    private readonly AuthorizationWindow _window;
+
+    public AuthPage(AuthorizationWindow window)
+    {
+        InitializeComponent();
+        _window = window;
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        this._window.Close();
+    }
+
+    private void MinimizedButton_Click(object sender, RoutedEventArgs e)
+    {
+        this._window.WindowState = WindowState.Minimized;
+    }
+
+    private void ToggleButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (this._window.WindowState == WindowState.Maximized)
+        {
+            this._window.WindowState = WindowState.Normal;
+            this._window.Width = 1150;
+            this._window.Height = 800;
+        }
+        else
+        {
+            this._window.WindowState = WindowState.Maximized;
+        }
+    }
+}
