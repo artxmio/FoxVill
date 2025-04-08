1851422ce621c56703687296101f01d2e8032a32
artxmix <artem.artusevskij01@gmail.com>
2025-04-08 11:06:40 +0300

    Добавил регистрацию нового пользователя

---
+++ AutorizaitionServices/RegistrationService.cs
@@ -0,0 +1,41 @@
+﻿using FoxVill.DataBase;
+using FoxVill.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Windows;
+
+namespace FoxVill.AutorizaitionServices;
+
+public static class RegistrationService
+{
+    /// <summary>
+    /// Add new user in database.
+    /// return true, where adding was successful, otherwise return false
+    /// </summary>
+    /// <param name="newUser"></param>
+    /// <returns></returns>
+    public static async Task<bool> AddUserToDatabase(User newUser)
+    {
+        ArgumentNullException.ThrowIfNull(newUser);
+
+        using DatabaseContext context = new();
+
+        context.Users.Load();
+
+        var users = context.Users.Local.ToList();
+
+        if (users.Any(u => u.Email == newUser.Email))
+        {
+            MessageBox.Show("Такой пользователь уже существует!", "Внимание!");
+            return false;
+        }
+        else
+        {
+            await context.AddAsync(newUser);
+            context.SaveChanges();
+
+            MessageBox.Show("Регистрация прошла успешно!", "Внимание!");
+
+            return true;
+        }
+    }
+}
--- ViewModel/AuthorizationWindowViewModel.cs
+++ ViewModel/AuthorizationWindowViewModel.cs
@@ -1,12 +1,30 @@
-﻿using FoxVill.Model;
+﻿using FoxVill.AutorizaitionServices;
+using FoxVill.Command;
+using FoxVill.DataBase;
+using FoxVill.Model;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace FoxVill.ViewModel;
 
 public class AuthorizationWindowViewModel : INotifyPropertyChanged
 {
     private User _authUser = new();
+    private User _regUser = new();
+
+    private string _errorRegistrionMessage = string.Empty;
+    private string _repeatedPassword = string.Empty;
+
+    public string ErrorRegistrionMessage
+    {
+        get => _errorRegistrionMessage;
+        set
+        {
+            _errorRegistrionMessage = value;
+            OnPropertyChanged();
+        }
+    }
 
     public User AuthUser
     {
@@ -18,9 +36,6 @@ public class AuthorizationWindowViewModel : INotifyPropertyChanged
             OnPropertyChanged();
         }
     }
-
-    private User _regUser = new();
-
     public User RegUser
     {
         get => _regUser;
@@ -31,11 +46,59 @@ public class AuthorizationWindowViewModel : INotifyPropertyChanged
         }
     }
 
-    public string RepeatedPassword { get; set; } = "";
+    public string RegistrationEmail
+    {
+        get => _regUser.Email;
+        set
+        {
+            _regUser.Email = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(ErrorRegistrionMessage));
+        }
+    }
+    public string RegistrationPassword
+    {
+        get => _regUser.Password;
+        set
+        {
+            _regUser.Password = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(ErrorRegistrionMessage));
+        }
+    }
+    public string RepeatedPassword
+    {
+        get => _repeatedPassword;
+        set
+        {
+            _repeatedPassword = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(ErrorRegistrionMessage));
+        }
+    }
+
+    public ICommand RegistrationCommand { get; }
 
     public AuthorizationWindowViewModel()
     {
+        RegistrationCommand = new RelayCommand(async p => await RegistrateNewUser());
+    }
 
+    private async Task RegistrateNewUser()
+    {
+        if (string.IsNullOrEmpty(RegUser.Email) && string.IsNullOrEmpty(RegUser.Password))
+        {
+            ErrorRegistrionMessage = "Заполните все поля!";
+            return;
+        }
+
+        if (RegistrationPassword != RepeatedPassword)
+        {
+            ErrorRegistrionMessage = "Пароли не совпадают!";
+            return;
+        }
+
+        await RegistrationService.AddUserToDatabase(RegUser);
     }
 
     public event PropertyChangedEventHandler? PropertyChanged;
